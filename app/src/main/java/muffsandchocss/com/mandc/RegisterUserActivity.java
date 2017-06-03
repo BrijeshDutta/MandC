5cce26ed675e87ac44006d062f483bc2ed617ac9
Brijesh Dutta <brijeshdtt@gmail.com>
2017-06-03 13:17:21 +0530

    Updated RegisterUserActivity and UpdateUserProfileActivity-

--- app/src/main/java/muffsandchocss/com/mandc/RegisterUserActivity.java
+++ app/src/main/java/muffsandchocss/com/mandc/RegisterUserActivity.java
@@ -3,6 +3,7 @@ package muffsandchocss.com.mandc;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -66,6 +67,7 @@ public class RegisterUserActivity extends AppCompatActivity implements LoaderCal
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        final Intent intentUpdateUserProfile = new Intent(this,UpdateUserProfileActivity.class);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -82,15 +84,17 @@ public class RegisterUserActivity extends AppCompatActivity implements LoaderCal
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+
+
+        Button btnSignUp = (Button) findViewById(R.id.btnRegisterUserActivitySignUp);
+        btnSignUp.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                startActivity(intentUpdateUserProfile);
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
+        mLoginFormView = findViewById(R.id.register_form);
         mProgressView = findViewById(R.id.login_progress);
     }
 
