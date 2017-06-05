0e8bd35d73b8521c93a1ae743b901a9f7a0c9e2a
Brijesh Dutta <brijeshdtt@gmail.com>
2017-06-05 11:29:42 +0530

    2. Design place order screen

--- app/src/main/java/muffsandchocss/com/fragments/PlaceOrderFragment.java
+++ app/src/main/java/muffsandchocss/com/fragments/PlaceOrderFragment.java
@@ -8,22 +8,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import muffsandchocss.com.mandc.R;
 
 public class PlaceOrderFragment extends Fragment {
 
+    String [] SPINNER_LIST_DISH = {"Choclates","Sub way","Burger"};
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Place Order");
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place_order, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_place_order, container, false);
+
+        MaterialBetterSpinner materialBetterSpinner = (MaterialBetterSpinner)fragmentView.findViewById(R.id.dish);
+        /// / Inflate the layout for this fragment
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_dropdown_item_1line, SPINNER_LIST_DISH);
+        //adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        materialBetterSpinner.setAdapter(adapter);
+
+
+       return  fragmentView;
+
     }
 
 }
