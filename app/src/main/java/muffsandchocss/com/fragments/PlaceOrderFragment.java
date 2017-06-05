5317c5d9d97861d47600867d18db338df6ee0143
Brijesh Dutta <brijeshdtt@gmail.com>
2017-06-05 18:14:24 +0530

    Dry fruits multi select functionality

--- app/src/main/java/muffsandchocss/com/fragments/PlaceOrderFragment.java
+++ app/src/main/java/muffsandchocss/com/fragments/PlaceOrderFragment.java
@@ -1,22 +1,36 @@
 package muffsandchocss.com.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
+
+import java.util.ArrayList;
 
 import muffsandchocss.com.mandc.R;
 
 public class PlaceOrderFragment extends Fragment {
 
+
     String [] SPINNER_LIST_DISH = {"Choclates","Sub way","Burger"};
+
+    //Dry fruit selection
+    EditText editTextSelectDryFruits;
+
+    String [] listDryFruits;
+    boolean [] checkedDryFruits;
+    ArrayList<Integer> userSelectedDryFruits = new ArrayList<>();
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -28,17 +42,90 @@ public class PlaceOrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_place_order, container, false);
+        //Show dish drop down
+        showDishDropDown(fragmentView);
 
-        MaterialBetterSpinner materialBetterSpinner = (MaterialBetterSpinner)fragmentView.findViewById(R.id.dish);
-        /// / Inflate the layout for this fragment
+        //Show Dry fruits selection
+        showDryFruitSelection(fragmentView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_dropdown_item_1line, SPINNER_LIST_DISH);
-        //adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        materialBetterSpinner.setAdapter(adapter);
-
-
-       return  fragmentView;
+        return  fragmentView;
 
     }
+
+    private void showDishDropDown(View fragmentView) {
+
+        //Drop down to select type of dish
+        MaterialBetterSpinner materialBetterSpinner = (MaterialBetterSpinner)fragmentView.findViewById(R.id.dish);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_dropdown_item_1line, SPINNER_LIST_DISH);
+        materialBetterSpinner.setAdapter(adapter);
+    }
+
+    private void showDryFruitSelection(View fragmentView){
+
+        editTextSelectDryFruits = (EditText)fragmentView.findViewById(R.id.selectDryFruits);
+        //Creating list item
+        listDryFruits = getResources().getStringArray(R.array.dryfruit_list);
+        checkedDryFruits = new boolean[listDryFruits.length];
+
+        editTextSelectDryFruits.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    final AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                    alertDialog.setTitle(R.string.title_select_dryfruits);
+                    alertDialog.setMultiChoiceItems(listDryFruits, checkedDryFruits, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                            if (isChecked){
+
+                                //Check o avoid duplication
+                                if (!userSelectedDryFruits.contains(which) ){
+                                    //add the the checked value
+                                    userSelectedDryFruits.add(which);
+                                } else {
+                                    userSelectedDryFruits.remove(which);
+                                }
+                            }
+                        }
+                    });
+                    alertDialog.setCancelable(false);
+                    alertDialog.setPositiveButton(R.string.ok_label, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String item ="";
+                            for (int i = 0; i<userSelectedDryFruits.size();i++){
+                                item = item + listDryFruits[userSelectedDryFruits.get(i)];
+                                if (i != userSelectedDryFruits.size()-1){
+                                    item = item + ", ";
+                                }
+                            }
+                            editTextSelectDryFruits.setText(item);
+                        }
+                    });
+                    alertDialog.setNegativeButton(R.string.dismiss_label, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.setNeutralButton(R.string.clearall_label, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            for (int i = 0 ; i < checkedDryFruits.length; i++){
+                                checkedDryFruits [i] = false;
+                                userSelectedDryFruits.clear();
+                                editTextSelectDryFruits.setText("");
+                            }
+                        }
+                    });
+                    AlertDialog alertDialog1 = alertDialog.create();
+                    alertDialog1.show();
+                    Toast.makeText(getActivity(), "got the focus", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 
 }
