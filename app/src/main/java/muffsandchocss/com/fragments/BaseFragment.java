0f3bf90cd18a20ad21cb36319daad9d3efc79647
Brijesh Dutta <brijeshdtt@gmail.com>
2017-06-08 18:52:04 +0530

    1. Added buttons to the base fragment

--- app/src/main/java/muffsandchocss/com/fragments/BaseFragment.java
+++ app/src/main/java/muffsandchocss/com/fragments/BaseFragment.java
@@ -4,18 +4,41 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import muffsandchocss.com.mandc.R;
 
 
 public class BaseFragment extends Fragment {
+
+    Button buttonOrderChoclates;
+
+    Fragment fragment;
+    FragmentTransaction fragmentTransaction;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_base, container, false);
+
+        final View fragmentView = inflater.inflate(R.layout.fragment_base, container, false);
+
+        buttonOrderChoclates = (Button) fragmentView.findViewById(R.id.btnOrderChoclates);
+        buttonOrderChoclates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new PlaceOrderFragment();
+                if (fragment !=null){
+
+                    fragmentTransaction = getFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content_frame,fragment);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+        return fragmentView;
     }
 }
