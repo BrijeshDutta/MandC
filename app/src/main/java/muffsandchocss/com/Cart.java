aff7ac4cf7ffa0fa280d4a9319dc48948a24a0e3
Brijesh Dutta <brijeshdtt@gmail.com>
2017-09-06 13:15:24 +0530

    Redesign -Add to cart

--- app/src/main/java/muffsandchocss/com/Cart.java
+++ app/src/main/java/muffsandchocss/com/Cart.java
@@ -1,12 +1,21 @@
 package muffsandchocss.com;
 
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,8 +27,10 @@ import java.util.Locale;
 import info.hoang8f.widget.FButton;
 import muffsandchocss.com.Database.Database;
 import muffsandchocss.com.ViewHolder.CartAdapter;
+import muffsandchocss.com.common.Common;
 import muffsandchocss.com.mandc.R;
 import muffsandchocss.com.model.Order;
+import muffsandchocss.com.model.Request;
 
 public class Cart extends AppCompatActivity {
 
@@ -31,7 +42,7 @@ public class Cart extends AppCompatActivity {
 
     //Firebase
     FirebaseDatabase database;
-    DatabaseReference request;
+    DatabaseReference requests;
 
     List<Order> cart = new ArrayList<>();
     CartAdapter cartAdapter;
@@ -44,7 +55,7 @@ public class Cart extends AppCompatActivity {
         //Init Firebase
 
         database = FirebaseDatabase.getInstance();
-        request = database.getReference("Requests");
+        requests = database.getReference("Requests");
 
         //Init
         recyclerView = (RecyclerView) findViewById(R.id.listCart);
@@ -54,9 +65,74 @@ public class Cart extends AppCompatActivity {
 
         txtTotalPrice = (TextView) findViewById(R.id.total);
         btnPlaceOrder = (FButton) findViewById(R.id.btnPlaceOrder);
+        btnPlaceOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showAlertDialog();
+
+            }
+        });
 
         loadListFood();
-        
+
+    }
+
+    private void showAlertDialog() {
+
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(Cart.this);
+        alertDialog.setTitle("One more step");
+        alertDialog.setMessage("Enter your address : ");
+        final EditText edtAddress = new EditText(Cart.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        edtAddress.setLayoutParams(lp);
+        alertDialog.setView(edtAddress);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart);
+
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if (cart.size()<=0){
+                    Toast.makeText(Cart.this,"NO items in the cart!!",Toast.LENGTH_SHORT).show();
+                }else {
+                    //Create new Request
+
+                    Request request = new Request(
+                            Common.currentUser.getPhone(),
+                            Common.currentUser.getName(),
+                            edtAddress.getText().toString(),
+                            txtTotalPrice.getText().toString(),
+                            cart);
+                    //Submit to firebase
+                    requests.child(String.valueOf(System.currentTimeMillis()))
+                            .setValue(request).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                new Database(getBaseContext()).clearCart();
+                                Toast.makeText(Cart.this,"Thank You !! order placed successfully !",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+
+
     }
 
     private void loadListFood() {
@@ -69,7 +145,7 @@ public class Cart extends AppCompatActivity {
 
         for (Order order:cart){
             total+= (Integer.parseInt(order.getPrice()))*(Integer.parseInt(order.getQuantity()));
-            Locale locale = new Locale("en","US");
+            Locale locale = new Locale("en","in");
             NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
             txtTotalPrice.setText(fmt.format(total));
         }
